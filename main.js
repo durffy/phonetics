b6c66359a1bbee6172a7331c633922e8c533851b
cjd <connor_duffy@hotmail.com>
2021-06-29 10:40:54 -0700

    -test

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
-var NatoAlphabet = {"a":"Alpha",
-	"b":"Bravo", 
-	"c":"Charlie", 
-	"d":"Delta", 
-	"e":"Echo", 
-	"f":"Foxtrot", 
-	"g":"Golf", 
-	"h":"Hotel", 
-	"i":"India", 
-	"j":"Juliett", 
-	"k":"Kilo", 
-	"l":"Lima", 
-	"m":"Mike", 
-	"n":"November",
-	"o":"Oscar", 
-	"p":"Papa", 
-	"q":"Quebec", 
-	"r":"Romeo", 
-	"s":"Siera", 
-	"t":"Tango", 
-	"u":"Uniform", 
-	"v":"Victor",
-	"w":"Wiskey", 
-	"x":"X-ray", 
-	"y":"Yankee", 
-	"z":"Zulu"};
+import {NatoAlphabet} from "./alphabet.js";   
+console.log(NatoAlphabet);
 	
 var textToTranscribe = "";
 var transcribedOutput = document.getElementById("transcribedOutput");
