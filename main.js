e0a8003c371dbcf8413578e8a6f3eb2d4eef377c
cjd <connor_duffy@hotmail.com>
2021-07-12 15:53:21 -0700

    - correcting issues with alphabet

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-import {NatoAlphabet} from "./alphabet.js";   
-console.log(NatoAlphabet);
-	
+
+var NatoAlphabet = {
+	"a": "Alpha",
+	"b": "Bravo",
+	"c": "Charlie",
+	"d": "Delta",
+	"e": "Echo",
+	"f": "Foxtrot",
+	"g": "Golf",
+	"h": "Hotel",
+	"i": "India",
+	"j": "Juliett",
+	"k": "Kilo",
+	"l": "Lima",
+	"m": "Mike",
+	"n": "November",
+	"o": "Oscar",
+	"p": "Papa",
+	"q": "Quebec",
+	"r": "Romeo",
+	"s": "Siera",
+	"t": "Tango",
+	"u": "Uniform",
+	"v": "Victor",
+	"w": "Wiskey",
+	"x": "X-ray",
+	"y": "Yankee",
+	"z": "Zulu"
+  };
+
 var textToTranscribe = "";
 var transcribedOutput = document.getElementById("transcribedOutput");
 var arrayToTranscribe = []
